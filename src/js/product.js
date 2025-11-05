58dc95fe86ba59d488624933bfe3716a29694799
Francky Ludovic Ntyam Adjomo <francky.ntyam@gmail.com>
2025-11-05 21:01:37 +0100

    add item to the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-
+// use setLocalStorage imported from utils.mjs
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-  cart.push(product);
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart");
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -18,3 +18,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+  
